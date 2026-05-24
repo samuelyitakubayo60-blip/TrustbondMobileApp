bcf4d1d5938fe0d2c36334e4ae94bbbd5949cf93
dukuzejean09 <dukuzejean09@gmail.com>
2026-05-24 17:31:18 +0200

    ok

--- TrustBond/lib/services/api_service.dart
+++ TrustBond/lib/services/api_service.dart
@@ -2,6 +2,7 @@
 
 import 'dart:async';
 import 'dart:convert';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import '../config/api_config.dart';
